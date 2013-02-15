20fda2394057e7d33c2011127617c242aae0f914
Yanko Simeonoff <yanko@zecho.info>
2013-02-15 13:29:54 +0200

    Added 'condition' parameter to generator

--- data/generator/sfPropelModule/twitter/template/templates/_list_td_actions.php
+++ data/generator/sfPropelModule/twitter/template/templates/_list_td_actions.php
@@ -1,26 +1,33 @@
 <td>
-  <ul class="table-action">
-<?php foreach ($this->configuration->getValue('list.object_actions') as $name => $params): ?>
-<?php if ('_delete' == $name): ?>
-    <?php echo $this->addCredentialCondition('[?php echo $helper->linkToDelete($'.$this->getSingularName().', '.$this->asPhp($params).') ?]', $params) ?>
+    <ul class="table-action">
+        <?php foreach ($this->configuration->getValue('list.object_actions') as $name => $params): ?>
+        <?php if ('_delete' == $name): ?>
+            <?php echo $this->addCredentialCondition('[?php echo $helper->linkToDelete($' . $this->getSingularName() . ', ' . $this->asPhp($params) . ') ?]', $params) ?>
 
-<?php elseif ('_edit' == $name): ?>
-    <?php echo $this->addCredentialCondition('[?php echo $helper->linkToEdit($'.$this->getSingularName().', '.$this->asPhp($params).') ?]', $params) ?>
+            <?php elseif ('_edit' == $name): ?>
+            <?php echo $this->addCredentialCondition('[?php echo $helper->linkToEdit($' . $this->getSingularName() . ', ' . $this->asPhp($params) . ') ?]', $params) ?>
 
-<?php elseif ('_show' == $name): ?>
-    <?php echo $this->addCredentialCondition('[?php echo $helper->linkToShow($'.$this->getSingularName().', '.$this->asPhp($params).') ?]', $params) ?>
+            <?php elseif ('_show' == $name): ?>
+            <?php echo $this->addCredentialCondition('[?php echo $helper->linkToShow($' . $this->getSingularName() . ', ' . $this->asPhp($params) . ') ?]', $params) ?>
 
-<?php else: ?>
-    <li class="sf_admin_action_<?php echo $params['class_suffix'] ?>">
-      <?php
-        if (sfTwitterBootstrap::getProperty('use_icons_in_button', false)) {
-          $params['label'] = isset($params['icon']) ? '<i class="'.$params['icon'].'"></i> ' . $params['label'] : $params['label'];
-        }
-        echo $this->addCredentialCondition($this->getLinkToAction($name, $params, true), $params)
-      ?>
-
-    </li>
-<?php endif; ?>
-<?php endforeach; ?>
-  </ul>
+            <?php else: ?>
+            <?php if (isset($params['condition'])): ?>
+                [?php if
+                ( <?php echo (isset($params['condition']['invert']) && $params['condition']['invert'] ? '!' : '') . '$' . $this->getSingularName() . '->' . $params['condition']['function'] ?>
+                ( <?php echo (isset($params['condition']['params']) ? $params['condition']['params'] : '') ?> ) ):  ?]
+                <?php endif; ?>
+            <li class="sf_admin_action_<?php echo $params['class_suffix'] ?>">
+                <?php
+                if (sfTwitterBootstrap::getProperty('use_icons_in_button', false)) {
+                    $params['label'] = isset($params['icon']) ? '<i class="' . $params['icon'] . '"></i> ' . $params['label'] : $params['label'];
+                }
+                echo $this->addCredentialCondition($this->getLinkToAction($name, $params, true), $params)
+                ?>
+                <?php if (isset($params['condition'])): ?>
+                [?php endif; ?]
+                <?php endif; ?>
+            </li>
+            <?php endif; ?>
+        <?php endforeach; ?>
+    </ul>
 </td>
